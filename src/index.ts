1ad10fbc55e8cc864b5f7daea317f99da182c6a7
choi hoon chul <hosoon2@gmail.com>
2020-12-24 01:27:24 +0900

    composite 에그타르트

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {
   Game,
   CourtSession
 } from "./create";
-import { ShipAdapter, bridgeAdapter } from "./structural";
+import { ShipAdapter, bridgeAdapter, 레시피 } from "./structural";
 
 // [추상팩토리] Client: AbstractFactory 클래스에 선언된 인터페이스를 사용한다.
 const a1 = new CourtSession(new LannisterFactory());
@@ -20,12 +20,24 @@ const wall = Wall.getInstance();
 const jj = new Lannister();
 jj.age = 13;
 const dd = jj.clone();
-// [adapter] implementation
+// [adapter] implements
 const ship = new ShipAdapter();
 ship.turnLeft();
-// [bridgeAdapter] implementation
+// [bridgeAdapter] implements
 [
   new bridgeAdapter.DrownedGodAdapter(),
   new bridgeAdapter.OldGodsAdapter(),
   new bridgeAdapter.SevenGodsAdapter()
 ].forEach((inst) => inst.prayTo());
+console.clear();
+// [composite] implements
+const 우유 = new 레시피.재료("우유", 12, 23);
+const 달걀 = new 레시피.재료("달걀", 4, 33);
+const 박력분 = new 레시피.재료("박력분", 0, 3);
+
+const 에그타르트 = new 레시피.요리("에그타르트");
+// builder implements
+에그타르트.create([우유, 달걀, 박력분]);
+
+console.log(`${에그타르트.이름} 총 칼로리는 ${에그타르트.getCalories()}이다.`);
+console.log(`${에그타르트.이름} 들어간 재료는 ${에그타르트.getItem()}이다.`);
--- src/structural.ts
+++ src/structural.ts
@@ -26,12 +26,14 @@ const ShipAdapter = class {
 
 /**
  * bridge: 일관된 인터페이스를 제공하여 동일하게 취급할 수 있다.
- *
+ * 복잡한 일련의 객체들의 결합이 필요할때 유용
  */
 // interface
 const Religion = {
   OldGods: class {
-    prayTo() {}
+    prayTo(v: { name: string }) {
+      console.log(v.name);
+    }
   },
   DrownedGod: class {
     prayTo() {}
@@ -39,7 +41,12 @@ const Religion = {
   SevenGods: class {
     prayTo() {}
   },
-  희생: class {},
+  희생: class {
+    private name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+  },
   공양: class {}
 };
 
@@ -51,7 +58,7 @@ const bridgeAdapter = {
       this._oldGods = new Religion.OldGods();
     }
     prayTo() {
-      this._oldGods.prayTo(new Religion.희생());
+      this._oldGods.prayTo(new Religion.희생("희생"));
     }
   },
   DrownedGodAdapter: class {
@@ -69,5 +76,74 @@ const bridgeAdapter = {
     }
   }
 };
+/**
+ * composite: 트리구조에서 유용(dom)
+ * 자신과 같은 타입의 인스턴스를 포험할 수 있다.
+ * 주요 기능은 자식요소들과 컴포넌트의 상호교환이다.
+ */
+interface 구성 {
+  이름: string;
+  칼로리: number;
+  비타민: number;
+  getName(): string;
+  getCalories(): number;
+  getVitamin(): number;
+}
 
-export { ShipAdapter, bridgeAdapter };
+const 레시피 = {
+  // 리프노드
+  재료: class implements 구성 {
+    이름: string;
+    칼로리: number;
+    비타민: number;
+    constructor(a: string, b: number, c: number) {
+      this.이름 = a;
+      this.칼로리 = b;
+      this.비타민 = c;
+    }
+    getName() {
+      return this.이름;
+    }
+    getCalories() {
+      return this.칼로리;
+    }
+    getVitamin() {
+      return this.비타민;
+    }
+  },
+  요리: class {
+    이름: string;
+    재료: 구성[];
+    constructor(name: string) {
+      this.이름 = name;
+      this.재료 = [];
+    }
+    add(inst: 구성): void {
+      this.재료.push(inst);
+    }
+    // builder
+    create(item: 구성[]) {
+      item.forEach((v) => {
+        this.add(v);
+      });
+    }
+    getName(): string {
+      return this.이름;
+    }
+    getItem() {
+      return this.재료.map((v) => v.getName());
+    }
+    getCalories() {
+      return this.재료.reduce((p: number, n: 구성) => {
+        return p + n.getCalories();
+      }, 0);
+    }
+    getVitamin() {
+      return this.재료.reduce((p: number, n: 구성) => {
+        return p + n.getVitamin();
+      }, 0);
+    }
+  }
+};
+
+export { ShipAdapter, bridgeAdapter, 레시피 };
